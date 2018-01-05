a43e7c712dbb92f518791ec2ce1e2b7854686039
FelixG <felix.guerrero.clan@gmail.com>
2018-01-05 08:12:56 -0700

    from mac

--- routes/docker.js
+++ routes/docker.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/', (req, res, next) => {
   return res.json({
-    docker: 'rocks! because I get it'
+    docker: 'rocks! because I get it now'
   });
 });
 
