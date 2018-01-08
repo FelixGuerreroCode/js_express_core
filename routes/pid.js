9042e8f6a6e9f68a5214e9021c41d151e6e70c08
FelixG <felix.guerrero.clan@gmail.com>
2018-01-08 09:00:26 -0700

    Modified

---
+++ routes/pid.js
@@ -0,0 +1,18 @@
+var ps = require('ps-node');
+ 
+// A simple pid lookup 
+ps.lookup({ pid: 12345 }, function(err, resultList ) {
+    if (err) {
+        throw new Error( err );
+    }
+ 
+    var process = resultList[ 0 ];
+ 
+    if( process ){
+ 
+        console.log( 'PID: %s, COMMAND: %s, ARGUMENTS: %s', process.pid, process.command, process.arguments );
+    }
+    else {
+        console.log( 'No such process found!' );
+    }
+});
